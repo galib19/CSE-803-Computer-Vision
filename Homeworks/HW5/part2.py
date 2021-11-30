a98b9eaa854138185d84a98d39ef10f45fbbbdb5
galib19 <galibasa@msu.edu>
2021-11-29 22:25:15 -0500

    hw-5 intermediate

--- Homeworks/HW5/part2.py
+++ Homeworks/HW5/part2.py
@@ -54,8 +54,15 @@ class Network(nn.Module):
         super().__init__()
         # TODO: Design your own base module, define layers here
         self.base = nn.Sequential(
-            nn.Conv2d(1,32,5,1,2),
+            # nn.Conv2d(1,32,5,1,2),
+            # nn.ReLU(),
+            nn.Conv2d(1, 16, 5),
             nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(16, 32, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Linear(11, 11)
         )
         out_channel = 32 # TODO: Put the output channel number of your base module here
         self.avgpool = nn.AdaptiveAvgPool2d(1)
@@ -79,8 +86,8 @@ class Network(nn.Module):
         return x
     
 device = "cuda" if torch.cuda.is_available() else "cpu"
-trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
-testloader = DataLoader(testset, batch_size=64, shuffle=True)
+trainloader = DataLoader(trainset, batch_size=128, shuffle=True)
+testloader = DataLoader(testset, batch_size=128, shuffle=True)
 model = Network().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001) # TODO: Experiment with different optimizer
@@ -106,19 +113,39 @@ def train(model, loader, num_epoch = 10): # Train the model
 def evaluate(model, loader): # Evaluate accuracy on validation / test set
     model.eval() # Set the model to evaluation mode
     correct = 0
+    correct_idx = 0
+    correct_label = 0
     with torch.no_grad(): # Do not calculate grident to speed up computation
         for batch, label in tqdm(loader):
             batch = batch.to(device)
             label = label.to(device)
             pred = model(batch)
             correct += (torch.argmax(pred,dim=1)==label).sum().item()
+
     acc = correct/len(loader.dataset)
     print("Evaluation accuracy: {}".format(acc))
-    return acc
+    return acc, correct_idx, correct_label
 
 train(model, trainloader)
-evaluate(model, testloader)
-
-model.transfer() # Copy the weights from fc layer to 1x1 conv layer
 
 # TODO: Choose a correctly classified image and visualize it
+_, cidx, clabel = evaluate(model, testloader)
+cidx = 0
+for i in range(2, len(testset)):
+    pred = model(testset[i][0].unsqueeze(0).to(device))
+    if torch.argmax(pred).item() == testset[i][1].item():
+        cidx = i
+        break
+print(cidx)
+model.transfer() # Copy the weights from fc layer to 1x1 conv layer
+heatmap = model.visualize(testset[cidx][0].unsqueeze(0).to(device)).squeeze(0)
+print(heatmap.shape)
+# from google.colab import drive
+# drive.mount('/content/gdrive', force_remount=True)
+row1 = torch.cat(tuple([heatmap[i] for i in range(5)]), dim =1)
+row2 = torch.cat(tuple([heatmap[i] for i in range(5,10)]), dim =1)
+a = torch.cat((row1,row2), dim =0).cpu().detach().numpy()
+plt.imshow(testset[cidx][0].squeeze(0))
+plt.show()
+plt.imshow(a, interpolation='nearest')
+plt.show()
